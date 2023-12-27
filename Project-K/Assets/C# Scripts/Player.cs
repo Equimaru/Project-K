6c74fbebd20511ef89084e3215864b976c15a950
Equimaru <119777912+Equimaru@users.noreply.github.com>
2023-12-28 02:08:03 +0300

    Player Run and Idle animations

--- Project-K/Assets/C# Scripts/Player.cs
+++ Project-K/Assets/C# Scripts/Player.cs
@@ -1,47 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private GameInput gameInput;
-    public float collisionOffset = 0.05f;
-    public ContactFilter2D movementFilter;
+
+    public bool isRunning;
 
 
-
-    private Vector2 movementInput;
-
-    private Rigidbody2D rb;
-
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
-
-    private void FixedUpdate()
+    private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-        if (inputVector != Vector2.zero)
-        {
-            int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollisions,
-                moveSpeed + Time.fixedDeltaTime + collisionOffset);
+        Debug.Log(inputVector);
 
-            if (count == 0)
-            {
-                Vector3 MoveDir = new Vector3(inputVector.x, inputVector.y, 0f);
-                transform.position += MoveDir * moveSpeed * Time.fixedDeltaTime;
-            }
+
+        if (inputVector.x > 0f)
+        {
+            transform.localEulerAngles = new Vector3(0, 0, 0);
         }
+
+        if (inputVector.x < 0f)
+        {
+            transform.localEulerAngles = new Vector3(0, 180, 0);
+        }
+
+        Vector3 MoveDir = new Vector3(inputVector.x, inputVector.y / 2, 0f);
+        transform.position += MoveDir * moveSpeed * Time.deltaTime;
+
+        isRunning = MoveDir != Vector3.zero;
+
     }
 
-    
+    public bool IsRunning()
+    {
+        return isRunning;
+    }
 }
